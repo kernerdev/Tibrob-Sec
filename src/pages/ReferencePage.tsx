401c9e27e6cc1fa46b7b64667439a9a77be0a4cd
Bence Kerner <kernerdev@gmail.com>
2024-05-06 21:46:02 +0200

    update in lists

--- src/pages/ReferencePage.tsx
+++ src/pages/ReferencePage.tsx
@@ -9,29 +9,30 @@ function ReferencePage() {
     <h2 className="text-2xl font-bold mb-4">Referenciáink:</h2>
     <h3 className="text-xl font-bold mb-2">Állandó telephelyeink:</h3>
     <p className="mb-6">
-        <ul>
-          <li>TÖKÖL AIRPORT ZRT. /Antal Sebestyén vezérigazgató/ 2012-től A 420ha területű tököli repülőtér, melyen közel 80 vállalkozás telephelye/raktára található! </li>
-          <li>KEMATECHNIK INNOMONTAGE KFT. /Bognár Péter György ügyvezető/ 2020-tól </li>
-          <li>C.A.S. HOLDING ZRT. /Jóny István vezérigazgató/ 2018-2023 között</li>
+        <ul className="list-disc pl-2">
+            <li>TÖKÖL AIRPORT ZRT. /Antal Sebestyén vezérigazgató/ 2012-től A 420ha területű tököli repülőtér, melyen közel 80 vállalkozás telephelye/raktára található! </li>
+            <li>KEMATECHNIK INNOMONTAGE KFT. /Bognár Péter György ügyvezető/ 2020-tól </li>
+            <li>C.A.S. HOLDING ZRT. /Jóny István vezérigazgató/ 2018-2023 között</li>
         </ul>
     </p>
     <h3 className="text-xl font-bold mb-2">Rendezvényes partnereink 2012-től:</h3>
     <p>
-        <ul>
-          <li>NAG SYSTEMS KFT. /Szabó Norbert ügyvezető/ 2012-től</li>
-          <li>MAGYAR GYORSULÁSI SZÖVETSÉG /Kurtyán Ferenc elnök/ 2012-től </li>
-          <li>TE-SEC KFT. /Gódor László ügyvezető/ 2012-től </li>
-          <li>BUSZFESZTIVÁL /Édes Levente ügyvezető/ 2012-2014-ig drifteken a mai napig munkakapcsolatban vagyunk </li>
-          <li>BÁLINT ÁGNES KULTURÁLIS KÖZPONT (VECSÉS) /Kis Tóth János igazgató/2012-2015 </li>
-          <li>A TÖKÖLI REPÜLŐTÉR területén egész évben megrendezésre kerülő rendezvények (filmforgatások, Red Bull Air Race, céges partik) vagyonvédelmi biztosítása 2012-től </li>
-          <li>MAGYAR MOZIÜZEMELTETŐ KFT. /Kocsis Kitti ügyvezető/ 2017-2023 között </li>
-          <li>TATABÁNYAI MOZIÜZEMELTETŐ KFT. /Dravecz Bence ügyvezető/ 2019-től </li>
-          <li>LURDY RENDEZVÉNYSZERVEZŐ KFT. /Kocsis Kitti ügyvezető/ 2020-tól </li>
-          <li>PANNÓNIA MOVIE KFT. /Farkas Attila ügyvezető/ 2023-tól </li>
-          <li>SZÁPÁR KÖZSÉG ÖNKORMÁNYZATA /Trojákné Szita Katalin polgármester/ 2023-tól</li>
+        <ul className="list-disc pl-2">
+            <li>NAG SYSTEMS KFT. /Szabó Norbert ügyvezető/ 2012-től</li>
+            <li>MAGYAR GYORSULÁSI SZÖVETSÉG /Kurtyán Ferenc elnök/ 2012-től </li>
+            <li>TE-SEC KFT. /Gódor László ügyvezető/ 2012-től </li>
+            <li>BUSZFESZTIVÁL /Édes Levente ügyvezető/ 2012-2014-ig drifteken a mai napig munkakapcsolatban vagyunk </li>
+            <li>BÁLINT ÁGNES KULTURÁLIS KÖZPONT (VECSÉS) /Kis Tóth János igazgató/2012-2015 </li>
+            <li>A TÖKÖLI REPÜLŐTÉR területén egész évben megrendezésre kerülő rendezvények (filmforgatások, Red Bull Air Race, céges partik) vagyonvédelmi biztosítása 2012-től </li>
+            <li>MAGYAR MOZIÜZEMELTETŐ KFT. /Kocsis Kitti ügyvezető/ 2017-2023 között </li>
+            <li>TATABÁNYAI MOZIÜZEMELTETŐ KFT. /Dravecz Bence ügyvezető/ 2019-től </li>
+            <li>LURDY RENDEZVÉNYSZERVEZŐ KFT. /Kocsis Kitti ügyvezető/ 2020-tól </li>
+            <li>PANNÓNIA MOVIE KFT. /Farkas Attila ügyvezető/ 2023-tól </li>
+            <li>SZÁPÁR KÖZSÉG ÖNKORMÁNYZATA /Trojákné Szita Katalin polgármester/ 2023-tól</li>
         </ul>
     </p>
     </div>
+
 
     <Footer />
     </>
