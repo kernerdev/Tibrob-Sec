fba2e6a08cc6eed96d0c8239305daea8414b1e53
Bence Kerner <kernerdev@gmail.com>
2024-05-06 21:25:04 +0200

    update

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,54 +6,20 @@ function AboutPage() {
     <>
     <Header />
     <div className="mx-auto max-w-2xl lg:px-8">
-    <h2>A cég ügyvezető igazgatója:</h2>
-    <p>
-    Molnár Tibor: 1996-ban tettem vagyonőri vizsgát, azóta a vagyonvédelem szinte
-minden területén szereztem tapasztalatot. A kezdeti években őrként, majd később – az
-elvégzett munka minőségének, és az ebből fakadó kapcsolati tőkének, referenciáknak
-köszönhetően – koordinátorként, szolgálatvezetőként dolgoztam több nagy cégnél.
-A Tibrob-Sec Kft. abból a célból alakult, hogy a személyemhez köthető munkákat,
-kapcsolatokat teljes egészében én tudjam felügyelni.
-A tököli repülőtéren tizenkettedik éve dolgozom. A repülőteret őrző előző cégnél 3 évig
-szolgálatvezető voltam, de a tulajdonosok profilváltás miatt felmondták a munkát. A
-repülőtér vezetésével fennálló jó kapcsolatnak köszönhetően lehetőséget kaptam, hogy
-2013. január 1-jén cégem vegye át a tököli repülőtér őrzését. Ez a váltás a repülőtér
-működésében semmi fennakadást nem okozott.
+    <h2 className="text-2xl font-bold mb-4">A cég ügyvezető igazgatója:</h2>
+    <p className="mb-6">
+        Molnár Tibor: 1996-ban tettem vagyonőri vizsgát, azóta a vagyonvédelem szinte minden területén szereztem tapasztalatot. A kezdeti években őrként, majd később – az elvégzett munka minőségének, és az ebből fakadó kapcsolati tőkének, referenciáknak köszönhetően – koordinátorként, szolgálatvezetőként dolgoztam több nagy cégnél. A Tibrob-Sec Kft. abból a célból alakult, hogy a személyemhez köthető munkákat, kapcsolatokat teljes egészében én tudjam felügyelni. A tököli repülőtéren tizenkettedik éve dolgozom. A repülőteret őrző előző cégnél 3 évig szolgálatvezető voltam, de a tulajdonosok profilváltás miatt felmondták a munkát. A repülőtér vezetésével fennálló jó kapcsolatnak köszönhetően lehetőséget kaptam, hogy 2013. január 1-jén cégem vegye át a tököli repülőtér őrzését. Ez a váltás a repülőtér működésében semmi fennakadást nem okozott.
     </p>
-    <h2>A cég tulajdonosa:</h2>
-    <p>
-    Kerner Balázs: Cégünk létrehozása előtt elsősorban rendezvények biztosításával
-foglalkoztam. A vagyonőri vizsgát 1997-ben tettem le. 1998-ban végeztem Varga
-Miklósnál az I.B.S.S.A. Nemzetközi Testőrszövetség Professzionális Személyvédelmi
-Szakán, 2011-ben pedig a biztonságszervezői 1-es fokozatának szakképesítést is
-megszereztem. Többéves vagyonőri, középvezetői, és ügyvezetői szakmai gyakorlattal
-rendelkezem, melyet 2001 óta különböző, vagyonvédelmi társaságoknál szereztem meg.
+    <h2 className="text-2xl font-bold mb-4">A cég tulajdonosa:</h2>
+    <p className="mb-6">
+        Kerner Balázs: Cégünk létrehozása előtt elsősorban rendezvények biztosításával foglalkoztam. A vagyonőri vizsgát 1997-ben tettem le. 1998-ban végeztem Varga Miklósnál az I.B.S.S.A. Nemzetközi Testőrszövetség Professzionális Személyvédelmi Szakán, 2011-ben pedig a biztonságszervezői 1-es fokozatának szakképesítést is megszereztem. Többéves vagyonőri, középvezetői, és ügyvezetői szakmai gyakorlattal rendelkezem, melyet 2001 óta különböző, vagyonvédelmi társaságoknál szereztem meg.
     </p>
-    <h2>Üzletpolitikánk:</h2>
+    <h2 className="text-2xl font-bold mb-4">Üzletpolitikánk:</h2>
     <p>
-      A megbízhatóság, rugalmasság, felelősségvállalás. Megrendelőinkhez való maximális
-      alkalmazkodás. Célunk egy a piaci elvárásoknak megfelelő, ütőképes, időtálló cég
-      létrehozása, melynek tevékenysége az ügyfelek megelégedettségén nyugszik.
-      A velünk dolgozó vagyonőrök jelentős részével többéves munkakapcsolatunk van,
-      melynek során megbizonyosodhattunk munkavégzésük hatékonyságáról. Az új
-      jelentkezők közül lehetőség szerint a szakmai gyakorlattal rendelkezőket alkalmazzuk,
-      de nem zárkózunk el attól, hogy a pályakezdők tapasztalt őreink irányításával
-      dolgozzanak. Az alkalmazás alapvető feltétele a vagyonőri igazolvány megléte, ezen
-      kívül a megfelelő fizikai és szellemi kondíció.
-      A vagyonvédelem több területén kívánunk jelen lenni. Amint a referenciáinkból is
-      kitűnik, tevékenységi körünkben a folyamatos, hosszú távú megrendelés mellett az
-      alkalmi megrendelések, rendezvénybiztosítások is megtalálhatók.
-      Ezen kívül partnercégeink bevonásával adósságkezelést, követelésbehajtást,
-      kintlévőségek ügyintézését, takarítási feladatokat, magánnyomozást, kamera- és
-      riasztórendszerek telepítését is vállalni tudjuk. Illetve teljes rendezvényeszköz parkkal
-      tudjuk vállalni a kültéri megbízásokat (kordon, járművek, rádiók, stb.).
-      Leendő partnereinkkel történő szerződéskötésekre, illetve a megbízási díjak
-      kialakítására személyes tárgyalásokon, a megbízatás tárgyát képező feladatok és
-      elvárások részletes megbeszélése után kerül sor.
-      Reméljük, cégismertetőnk felkeltette az érdeklődését! Amennyiben további információra
-      van szüksége, személyesen állunk rendelkezésére.
+        A megbízhatóság, rugalmasság, felelősségvállalás. Megrendelőinkhez való maximális alkalmazkodás. Célunk egy a piaci elvárásoknak megfelelő, ütőképes, időtálló cég létrehozása, melynek tevékenysége az ügyfelek megelégedettségén nyugszik. A velünk dolgozó vagyonőrök jelentős részével többéves munkakapcsolatunk van, melynek során megbizonyosodhattunk munkavégzésük hatékonyságáról. Az új jelentkezők közül lehetőség szerint a szakmai gyakorlattal rendelkezőket alkalmazzuk, de nem zárkózunk el attól, hogy a pályakezdők tapasztalt őreink irányításával dolgozzanak. Az alkalmazás alapvető feltétele a vagyonőri igazolvány megléte, ezen kívül a megfelelő fizikai és szellemi kondíció. A vagyonvédelem több területén kívánunk jelen lenni. Amint a referenciáinkból is kitűnik, tevékenységi körünkben a folyamatos, hosszú távú megrendelés mellett az alkalmi megrendelések, rendezvénybiztosítások is megtalálhatók. Ezen kívül partnercégeink bevonásával adósságkezelést, követelésbehajtást, kintlévőségek ügyintézését, takarítási feladatokat, magánnyomozást, kamera- és riasztórendszerek telepítését is vállalni tudjuk. Illetve teljes rendezvényeszköz parkkal tudjuk vállalni a kültéri megbízásokat (kordon, járművek, rádiók, stb.). Leendő partnereinkkel történő szerződéskötésekre, illetve a megbízási díjak kialakítására személyes tárgyalásokon, a megbízatás tárgyát képező feladatok és elvárások részletes megbeszélése után kerül sor. Reméljük, cégismertetőnk felkeltette az érdeklődését! Amennyiben további információra van szüksége, személyesen állunk rendelkezésére.
     </p>
-    </div>
+</div>
+
     <Footer />
     </>
   );
--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -6,22 +6,20 @@ function ContactsPage() {
     <>
     <Header />
     <div className="mx-auto max-w-2xl lg:px-8">
-    <p>
-      Kapcsolat:
-      KERNER BALÁZS - tulajdonos
-      Tibrob-Sec Kft.
-      Tel.: +36-30/300-6131
-      e-mail: tibrobsec@tibrobsec.hu
-    </p>
-    </div>
-    <div className="mx-auto max-w-2xl lg:px-8">
-    <p>
-      Kapcsolat:
-      Molnár Tibor - ügyvezető
-      Tibrob-Sec Kft.
-      Tel.: +36-70/397-8990
-      e-mail: tibrobsec@tibrobsec.hu 
-    </p>
+      <div className="mb-4">
+          <p>Kapcsolat:</p>
+          <p className="font-bold">Kerner Balázs - tulajdonos</p>
+          <p>Tibrob-Sec Kft.</p>
+          <p>Tel.: <a href="tel:+36303006131">+36-30/300-6131</a></p>
+          <p>e-mail: <a href="mailto:tibrobsec@tibrobsec.hu">tibrobsec@tibrobsec.hu</a></p>
+      </div>
+      <div className="mb-4">
+          <p>Kapcsolat:</p>
+          <p className="font-bold">Molnár Tibor - ügyvezető</p>
+          <p>Tibrob-Sec Kft.</p>
+          <p>Tel.: <a href="tel:+36703978990">+36-70/397-8990</a></p>
+          <p>e-mail: <a href="mailto:tibrobsec@tibrobsec.hu">tibrobsec@tibrobsec.hu</a></p>
+      </div>
     </div>
     <Footer />
     </>
